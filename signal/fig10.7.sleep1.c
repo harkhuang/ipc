e08ffbcdbc591f292a3f9ea8f945d1203395a0e4
harkhuang <shiyanhk@gmail.com>
2017-12-05 17:49:49 +0800

    添加sleep函数理解

--- signal/fig10.7.sleep1.c
+++ signal/fig10.7.sleep1.c
@@ -14,22 +14,25 @@ sig_alrm(int signo)
 
 // 缺陷在哪里?
 // 1.sleep1 在使用前调用了alarm(5)这样会被函数里面第二次调用alarm覆盖
-// 2.alarm默认行为的修改  
-// 3.系统繁忙存在竞争  即存在延迟的情况 即alarm被处理后 一直挂在pause()
+// 2.alarm默认行为的修改
+// 3.系统繁忙存在竞争   pause 函数 和 alarm函数存在竞争  即存在延迟的情况 即alarm被处理后 一直挂在pause()
+
+
+//解决办法 (基于通信)
+// 1.利用返回值记录 剩余时钟
+// 2.修改信号掩码  对未决信号 做特殊处理
+// 3.利用setjmp和longjmp函数切换上下文
+
 unsigned int
 sleep1(unsigned int nsecs)
 {
 
 	// sleep(1);  // 添加这行函数看alarm函数返回值变化   5- 1
-	
 	//   SIGALRM默认处理方式：终止；当由alarm或setitimer函数设置的定时器超时时，会产生该信号。
 	// 这是一条用户态到内核态的转换 所有的apue函数都是涉及到内核的切换
 	if (signal(SIGALRM, sig_alrm) == SIG_ERR)  // 既然重新定义了该信号就要对该信号默认行为 所在系统的定义要有所了解
 		return(nsecs);
-	int ret  =  alarm(nsecs);		/* start the timer */
-
-	alarm(ret+nsecs);    //再次覆盖闹钟函数  
-	printf("alarm函数返回值 = %d\n",ret);
+	alarm(nsecs);		/* start the timer */
 	pause();			/* next caught signal wakes us up */
 	return(alarm(0));	/* turn off timer, return unslept time */
 }
@@ -40,9 +43,7 @@ sleep1(unsigned int nsecs)
 // 实际执行了1秒钟
 int main()
 {
-	// 预期执行五秒  但是sleep1实现代码中  却中断了 alarm 时钟  
-	// 多次调用时钟信号  后面的信号会覆盖前面的信号
-	alarm(5);  // 这里如果定义了 5秒 实际睡眠只有两秒 
+	alarm(5);  // 问题1	:多次调用时钟信号  后面的信号会覆盖前面的信号
 	sleep1(1);
 	return 0;
 }
--- signal/fig10.8.sleep2.c
+++ signal/fig10.8.sleep2.c
@@ -7,24 +7,28 @@ static jmp_buf	env_alrm;
 static void
 sig_alrm(int signo)
 {
-	printf("我进入了信号回调函数 sig_alrm\n");
-
-	printf("我尝试了再次跳转到主调函数 \n");
+	printf("时钟信号来了,我进入了信号回调函数 sig_alrm\n");
+	printf("我尝试了longjmp(env_alrm, 1);再次跳转到主调函数 \n");
 	longjmp(env_alrm, 1);// 通常第二个参数都默认写成1
-	printf("跳出了 sig_alrm\n");
+	printf("跳出了 sig_alrm\n");//此行代码无效
 }
 
 
-//模拟sleep2
+
+
+// 避免了pause函数和alarm韩寒苏竞争
 unsigned int
 sleep2(unsigned int nsecs)
 {
-	if (signal(SIGALRM, sig_alrm) == SIG_ERR) // 定义用户级别信号动作
+	if (signal(SIGALRM, sig_alrm) == SIG_ERR)  
 		return(nsecs);
-	if (setjmp(env_alrm) == 0) { // 设置一个longjmp的标签  返回值为0认为是成功完成了设置
+	if (setjmp(env_alrm) == 0) { // 设置一个longjmp的标签  返回值为0表示成功完成了设置
 	   // 我设置了一个 
+	    printf("我成功的设置了 setjmp(env_alrm) \n");
 	    printf("我设置了一个 %d 秒的闹钟信号\n",nsecs);
-		alarm(nsecs);		/* start the timer */   // 来个闹钟实时 
+		alarm(nsecs);		/* start the timer */   // 来个闹钟实时  如果时钟信号来了  那么代码跳到25行重新来过 避免了alarm先调用 后面pause没法结束
+	    printf("成功定时向本进程发送一个时钟信号\n");
+		printf("在此pause等待时钟信号.......\n\n\n");
 		pause();			/* next caught signal wakes us up */
 	}
 	return(alarm(0));		/* turn off timer, return unslept time */
@@ -37,6 +41,9 @@ sleep2(unsigned int nsecs)
 // 利用函数跳转的方式  来屏蔽上下文中信号属性???
 // 防止内核态(繁忙)到用户态状态切换的错误
 
+
+
+//不懂为什么避免了竞争?
 int main()
 {
 	// 预期执行五秒  但是sleep1实现代码中  却中断了 alarm 时钟  
