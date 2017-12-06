b20b54a2b945d657f025c21ad1cd1f1b17f6b6dc
harkhuang <shiyanhk@gmail.com>
2017-12-06 17:04:09 +0800

    加深理解sleep函数实现

--- signal/fig10.8.sleep2.c
+++ signal/fig10.8.sleep2.c
@@ -4,31 +4,25 @@
 #include <stdio.h>
 static jmp_buf	env_alrm;
 
-static void
-sig_alrm(int signo)
-{
-	printf("时钟信号来了,我进入了信号回调函数 sig_alrm\n");
-	printf("我尝试了longjmp(env_alrm, 1);再次跳转到主调函数 \n");
+
+
+// 通过setjmp 返回值不同 和 longjmp来跳转
+static void sig_alrm(int signo){
 	longjmp(env_alrm, 1);// 通常第二个参数都默认写成1
-	printf("跳出了 sig_alrm\n");//此行代码无效
 }
-
-
-
-
-// 避免了pause函数和alarm韩寒苏竞争
+ 
 unsigned int
 sleep2(unsigned int nsecs)
 {
 	if (signal(SIGALRM, sig_alrm) == SIG_ERR)  
 		return(nsecs);
-	if (setjmp(env_alrm) == 0) { // 设置一个longjmp的标签  返回值为0表示成功完成了设置
-	   // 我设置了一个 
-	    printf("我成功的设置了 setjmp(env_alrm) \n");
-	    printf("我设置了一个 %d 秒的闹钟信号\n",nsecs);
+
+	// 这行代码的意义就是确定了  alarm和pause的先后顺序
+	// 1.执行了先完整调用alarm  然后调用pause
+	// 2.alarm没有完全调用(即 产生 ->未决->递送)完整的过程 
+	if (setjmp(env_alrm) == 0) { // 设置一个longjmp的标签  返回值为0表示成功完成了设置  如果返回值不是这个 那么就执行else分支
 		alarm(nsecs);		/* start the timer */   // 来个闹钟实时  如果时钟信号来了  那么代码跳到25行重新来过 避免了alarm先调用 后面pause没法结束
-	    printf("成功定时向本进程发送一个时钟信号\n");
-		printf("在此pause等待时钟信号.......\n\n\n");
+		sleep(5);          // 这里不怕竞争条件了  跳转后 完成了对竞争条件的处理
 		pause();			/* next caught signal wakes us up */
 	}
 	return(alarm(0));		/* turn off timer, return unslept time */
@@ -49,6 +43,7 @@ int main()
 	// 预期执行五秒  但是sleep1实现代码中  却中断了 alarm 时钟  
 	// 多次调用时钟信号  后面的信号会覆盖前面的信号
 	//alarm(5);  // 这里如果定义了 5秒 实际睡眠只有两秒 
-	sleep2(2);
+	int ret = sleep2(1);
+	printf("ret= %d\n",ret);
 	return 0;
 }
